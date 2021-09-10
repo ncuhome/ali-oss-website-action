37a387c1489ebc59add044eb2fc3ee59ef944b06
Zeb Wu <wzb3422@gmail.com>
2021-09-10 14:39:25 +0800

    chore: no emit d.ts

--- dist/upload.d.ts
+++
@@ -1,4 +0,0 @@
-import OSS from "ali-oss";
-import { walkdirCallback } from './walkdir';
-export declare function getUploadCallback(client: OSS, base: string): walkdirCallback;
-export declare function uploadFolder(client: OSS): Promise<void>;
