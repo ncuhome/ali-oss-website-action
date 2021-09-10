6fc133023c3412016e63c88d94febf91b0599202
Zeb Wu <wzb3422@gmail.com>
2021-09-10 12:46:41 +0800

    feat: directly use ts-node

--- src/upload.ts
+++ src/upload.ts
@@ -5,20 +5,20 @@ import path from 'path';
 import { getEnv } from './env';
 import chalk from "chalk";
 
-export function getUploadCallback(client: OSS): walkdirCallback {
-  console.log('Current working Directory is', process.cwd());
-  const uploadBase = path.resolve(process.cwd(), getEnv('FOLDER'));
+export function getUploadCallback(client: OSS, base: string): walkdirCallback {
+  console.log('Current working Directory is', process.env);
+
   let callback: walkdirCallback = async (filepath) => {
     if (fs.lstatSync(filepath).isFile()) {
       console.log(chalk.green('UPLOAD'), filepath);
-      await client.put(filepath.substr(uploadBase.length), filepath);
+      await client.put(filepath.substr(base.length), filepath);
     }
   }
   return callback;
 }
 
 export async function uploadFolder(client: OSS) {
-  const base = path.resolve(process.cwd(), getEnv('FOLDER'));
-  const callback = getUploadCallback(client);
-  const result = await walkdir(base, callback);
+  const uploadBase = path.resolve(process.cwd(), getEnv('FOLDER'));
+  const callback = getUploadCallback(client, uploadBase);
+  const result = await walkdir(uploadBase, callback);
 }
